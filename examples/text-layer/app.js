a0fde9e23187449155e8baccb3b6e01971d03df0
Xiaoji Chen <xiaoji@uber.com>
2018-04-17 21:33:01 -0700

    clean up text layer

--- examples/text-layer/app.js
+++ examples/text-layer/app.js
@@ -12,10 +12,6 @@ const MAPBOX_STYLE =
   'https://rivulet-zhang.github.io/dataRepo/mapbox/style/map-style-dark-v9-no-labels.json';
 // sample data
 const DATA_URL = 'https://rivulet-zhang.github.io/dataRepo/text-layer/hashtagsOneDayWithTime.json';
-const SECONDS_PER_DAY = 24 * 60 * 60;
-// visualize data within in the time window of [current - TIME_WINDOW, current + TIME_WINDOW]
-const TIME_WINDOW = 2;
-const TEXT_COLOR = [255, 200, 0];
 
 class Root extends Component {
   constructor(props) {
@@ -25,72 +21,29 @@ class Root extends Component {
         ...DeckGLOverlay.defaultViewport,
         width: 500,
         height: 500
-      }
+      },
+      currentTime: 0
     };
-    this._loadData();
+
+    this._animation = null;
+
+    this._resize = this._resize.bind(this);
+    this._onViewportChange = this._onViewportChange.bind(this);
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this._resize.bind(this));
+    window.addEventListener('resize', this._resize);
     this._resize();
 
-    this._loadData();
-  }
-
-  componentWillUnmount() {
-    if (this._animationFrame) {
-      window.cancelAnimationFrame(this._animationFrame);
-    }
+    fetch(DATA_URL)
+      .then(resp => resp.json())
+      .then(data => this.setState({data}));
   }
 
   _onViewportChange(viewport) {
     this.setState({
       viewport: {...this.state.viewport, ...viewport}
     });
-  }
-
-  _loadData() {
-    fetch(DATA_URL)
-      .then(resp => resp.json())
-      .then(resp => {
-        // each entry in the data object contains all tweets posted at that second
-        const data = Array.from({length: SECONDS_PER_DAY}, () => []);
-        resp.forEach(val => {
-          const second = parseInt(val.time, 10) % SECONDS_PER_DAY;
-          data[second].push(val);
-        });
-        this.setState({data});
-        window.requestAnimationFrame(this._animateData.bind(this));
-      });
-  }
-
-  _animateData() {
-    const {data} = this.state;
-    const now = Date.now();
-    const getSecCeil = ms => Math.ceil(ms / 1000, 10) % SECONDS_PER_DAY;
-    const getSecFloor = ms => Math.floor(ms / 1000, 10) % SECONDS_PER_DAY;
-    const timeWindow = [
-      getSecCeil(now - TIME_WINDOW * 1000),
-      getSecFloor(now + TIME_WINDOW * 1000)
-    ];
-    if (data) {
-      let dataSlice = [];
-      for (let i = timeWindow[0]; i <= timeWindow[1]; i++) {
-        if (i >= 0 && i < data.length) {
-          const slice = data[i].map(val => {
-            const offset = Math.abs(getSecFloor(now) + (now % 1000) / 1000 - i) / TIME_WINDOW;
-            // use non-linear function to achieve smooth animation
-            const opac = Math.cos(offset * Math.PI / 2);
-            const color = [...TEXT_COLOR, opac * 255];
-            return Object.assign({}, val, {color}, {size: 12 * (opac + 1)});
-          });
-          dataSlice = [...dataSlice, ...slice];
-        }
-      }
-      this.setState({dataSlice});
-    }
-
-    this._animationFrame = window.requestAnimationFrame(this._animateData.bind(this));
   }
 
   _resize() {
@@ -101,17 +54,16 @@ class Root extends Component {
   }
 
   render() {
-    const {viewport, dataSlice} = this.state;
+    const {viewport, data} = this.state;
 
     return (
       <MapGL
         {...viewport}
         mapStyle={MAPBOX_STYLE}
-        preventStyleDiffing={true}
-        onViewportChange={this._onViewportChange.bind(this)}
+        onViewportChange={this._onViewportChange}
         mapboxApiAccessToken={MAPBOX_TOKEN}
       >
-        <DeckGLOverlay viewport={viewport} data={dataSlice} />
+        <DeckGLOverlay viewport={viewport} data={data} isPlaying={true} time={0} />
       </MapGL>
     );
   }
--- examples/text-layer/deckgl-overlay.js
+++ examples/text-layer/deckgl-overlay.js
@@ -1,6 +1,14 @@
+/* global window */
 import React, {Component} from 'react';
-import {setParameters} from 'luma.gl';
 import DeckGL, {TextLayer} from 'deck.gl';
+
+const SECONDS_PER_DAY = 24 * 60 * 60;
+const TOTAL_SLICES = 24;
+const SECONDS_PER_SLICE = SECONDS_PER_DAY / TOTAL_SLICES;
+// visualize data within in the time window of [current - TIME_WINDOW, current]
+const TIME_WINDOW = 200;
+const INVISIBLE_COLOR = [0, 0, 0, 0];
+const ANIMATION_SPEED = 40;
 
 export default class DeckGLOverlay extends Component {
   static get defaultViewport() {
@@ -14,26 +22,135 @@ export default class DeckGLOverlay extends Component {
     };
   }
 
-  _initialize(gl) {
-    setParameters(gl, {
-      blendFunc: [gl.SRC_ALPHA, gl.ONE, gl.ONE_MINUS_DST_ALPHA, gl.ONE],
-      blendEquation: gl.FUNC_ADD
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSlices: this._sliceData(props.data),
+      currentTime: props.time
+    };
+
+    this._animate = this._animate.bind(this);
+
+    if (props.isPlaying) {
+      this._startAnimation();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data !== this.props.data) {
+      this.setState({
+        dataSlices: this._sliceData(nextProps.data)
+      });
+    }
+
+    if (nextProps.isPlaying !== this.props.isPlaying || nextProps.time !== this.props.time) {
+      if (nextProps.isPlaying) {
+        this._startAnimation();
+      } else {
+        this._stopAnimation();
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this._stopAnimation();
+  }
+
+  _animate() {
+    const deltaTime = (Date.now() - this._startTime) / 1000 * ANIMATION_SPEED;
+    const currentTime = (deltaTime + this.props.time) % SECONDS_PER_DAY;
+    this.setState({currentTime});
+    this._animation = window.requestAnimationFrame(this._animate);
+  }
+
+  _startAnimation() {
+    this._stopAnimation();
+    this._startTime = Date.now();
+    this._animation = window.requestAnimationFrame(this._animate);
+  }
+
+  _stopAnimation() {
+    window.cancelAnimationFrame(this._animation);
+  }
+
+  /**
+   * We don't want to render too many invisible objects
+   * but we also don't want to re-calculating text layout every frame
+   * Slice the input by time so that data array has a managable size yet only changes infrequently
+   */
+  _sliceData(data) {
+    if (!data) {
+      return null;
+    }
+
+    const dataSlices = Array.from({length: TOTAL_SLICES}, () => []);
+
+    data.forEach(d => {
+      const time = d.time % SECONDS_PER_DAY;
+
+      const sliceIndex = Math.floor(time / SECONDS_PER_SLICE);
+      dataSlices[sliceIndex].push(d);
+
+      // Items that appear at the end of each hour are still visible
+      // at the beginning of the next hour
+      const nextSliceIndex = Math.floor((time + TIME_WINDOW) / SECONDS_PER_SLICE);
+      if (nextSliceIndex !== sliceIndex && nextSliceIndex < TOTAL_SLICES) {
+        dataSlices[nextSliceIndex].push(d);
+      }
+      d.timeOfDay = time;
     });
+
+    return dataSlices;
+  }
+
+  /**
+   * Get the data slice at the current timestamp
+   */
+  _getCurrentData(dataSlices, currentTime) {
+    if (!dataSlices) {
+      return null;
+    }
+
+    const sliceIndex = Math.floor(currentTime / SECONDS_PER_SLICE);
+    const data = dataSlices[sliceIndex];
+
+    const fromTime = currentTime - TIME_WINDOW;
+
+    // pre-calculate the scaler inside the current time window
+    data.forEach(d => {
+      const time = d.timeOfDay;
+      if (time <= fromTime || time >= currentTime) {
+        d.r = 0;
+      } else {
+        d.r = Math.pow((time - fromTime) / TIME_WINDOW, 2);
+      }
+    });
+
+    return data;
   }
 
   render() {
-    const {viewport, data} = this.props;
+    const {viewport} = this.props;
+    const {dataSlices, currentTime} = this.state;
+    const data = this._getCurrentData(dataSlices, currentTime);
 
     const layers = [
       new TextLayer({
         id: 'hashtag-layer',
         data,
-        sizeScale: 8,
-        getColor: d => d.color,
-        getSize: d => d.size
+        sizeScale: 48,
+        getColor: d => (d.r ? [255 * d.r, 200, (1 - d.r) * 255, 255 * d.r] : INVISIBLE_COLOR),
+        getSize: d => d.r,
+        getPosition: d => d.coordinates,
+        updateTriggers: {
+          // update color and size iff currentTime changes
+          getColor: currentTime,
+          getSize: currentTime
+        }
       })
     ];
 
-    return <DeckGL {...viewport} layers={layers} onWebGLInitialized={this._initialize} />;
+    return <DeckGL {...viewport} layers={layers} />;
   }
 }
